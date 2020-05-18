d7406e22ba31781a60e466bc38bd92a177136416
Crawford Collins <crawford.collins@gmail.com>
2020-05-18 14:19:00 -0500

    blackened src/

--- srv/api.py
+++ srv/api.py
@@ -8,23 +8,20 @@ app = falcon.API()
 
 class RandomThree(object):
     def on_get(self, req, resp):
-        resp.set_header('Access-Control-Allow-Origin', '*')
+        resp.set_header("Access-Control-Allow-Origin", "*")
         with sqlite3.connect("file:data/inbrain.db?mode=ro", uri=True) as conn:
             dict_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
             conn.row_factory = dict_factory
 
-            result = conn.execute("SELECT * FROM stories ORDER BY RANDOM() LIMIT 3").fetchall()
-
-
-
-
+            result = conn.execute(
+                "SELECT * FROM stories ORDER BY RANDOM() LIMIT 3"
+            ).fetchall()
 
         resp.status = falcon.HTTP_200
-        body = {"results" : result}
+        body = {"results": result}
         resp.body = ujson.dumps(body)
-
 
 
 random_three = RandomThree()
 
-app.add_route("/random_three",random_three)
+app.add_route("/random_three", random_three)
